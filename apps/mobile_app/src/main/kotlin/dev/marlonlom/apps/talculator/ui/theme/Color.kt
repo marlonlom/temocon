280bc48c1246100108147db39ea83d501bdad533
Marlon Jose Lopez Meza <malm.gm@gmail.com>
2023-02-27 17:57:26 -0500

    feat: updated material brand colors

--- apps/mobile_app/src/main/kotlin/dev/marlonlom/apps/talculator/ui/theme/Color.kt
+++ apps/mobile_app/src/main/kotlin/dev/marlonlom/apps/talculator/ui/theme/Color.kt
@@ -10,10 +10,106 @@ package dev.marlonlom.apps.talculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/* Brand colors */
+private object BrandColors {
+  val armadillo = Color(0xFF43483F)
+  val bilbao = Color(0xFF356A21)
+  val bilbao2 = Color(0xFF3D691B)
+  val black = Color(0xFF000000)
+  val britishRacingGreen = Color(0xFF0C3900)
+  val britishRacingGreen2 = Color(0xFF133800)
+  val britishRacingGreen3 = Color(0xFF183800)
+  val crusoe = Color(0xFF1D5209)
+  val darkGreen = Color(0xFF052100)
+  val darkGreen2 = Color(0xFF082100)
+  val darkGreen3 = Color(0xFF0B2000)
+  val delta = Color(0xFF8D9387)
+  val feijoa = Color(0xFF9AD680)
+  val feijoa2 = Color(0xFFA1D579)
+  val feta = Color(0xFFDFE4D7)
+  val fireBrick = Color(0xFFBA1A1A)
+  val green = Color(0xFF265101)
+  val gunsmoke = Color(0xFF73796E)
+  val harp = Color(0xFFC3C8BB)
+  val ivory = Color(0xFFF7FFEC)
+  val maroon = Color(0xFF2B1700)
+  val maroon2 = Color(0xFF410002)
+  val maroon3 = Color(0xFF482900)
+  val maroon4 = Color(0xFF673D00)
+  val maroon5 = Color(0xFF690005)
+  val melon = Color(0xFFFFB4AB)
+  val mintGreen = Color(0xFFCEFFAE)
+  val mistyRose = Color(0xFFFFDAD6)
+  val olive = Color(0xFF885200)
+  val peachPuff = Color(0xFFFFDCBB)
+  val rajah = Color(0xFFFFB869)
+  val reef = Color(0xFFB5F399)
+  val reef2 = Color(0xFFB9F396)
+  val reef3 = Color(0xFFBCF293)
+  val sangria = Color(0xFF93000A)
+  val white = Color(0xFFFFFFFF)
+}
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+/* Light theme colors */
+val md_theme_light_onSurfaceVariant = BrandColors.armadillo
+val md_theme_light_primary = BrandColors.bilbao
+val md_theme_light_surfaceTint = BrandColors.bilbao
+val md_theme_light_secondary = BrandColors.bilbao2
+val md_theme_light_shadow = BrandColors.black
+val md_theme_light_scrim = BrandColors.black
+val md_theme_light_inverseSurface = BrandColors.britishRacingGreen2
+val md_theme_light_onPrimaryContainer = BrandColors.darkGreen
+val md_theme_light_onBackground = BrandColors.darkGreen2
+val md_theme_light_onSurface = BrandColors.darkGreen2
+val md_theme_light_onSecondaryContainer = BrandColors.darkGreen3
+val md_theme_light_inversePrimary = BrandColors.feijoa
+val md_theme_light_surfaceVariant = BrandColors.feta
+val md_theme_light_error = BrandColors.fireBrick
+val md_theme_light_outline = BrandColors.gunsmoke
+val md_theme_light_outlineVariant = BrandColors.harp
+val md_theme_light_background = BrandColors.ivory
+val md_theme_light_surface = BrandColors.ivory
+val md_theme_light_onTertiaryContainer = BrandColors.maroon
+val md_theme_light_onErrorContainer = BrandColors.maroon2
+val md_theme_light_inverseOnSurface = BrandColors.mintGreen
+val md_theme_light_errorContainer = BrandColors.mistyRose
+val md_theme_light_tertiary = BrandColors.olive
+val md_theme_light_tertiaryContainer = BrandColors.peachPuff
+val md_theme_light_primaryContainer = BrandColors.reef
+val md_theme_light_secondaryContainer = BrandColors.reef3
+val md_theme_light_onPrimary = BrandColors.white
+val md_theme_light_onSecondary = BrandColors.white
+val md_theme_light_onTertiary = BrandColors.white
+val md_theme_light_onError = BrandColors.white
+
+/* Dark theme colors */
+val md_theme_dark_surfaceVariant = BrandColors.armadillo
+val md_theme_dark_outlineVariant = BrandColors.armadillo
+val md_theme_dark_inversePrimary = BrandColors.bilbao
+val md_theme_dark_shadow = BrandColors.black
+val md_theme_dark_scrim = BrandColors.black
+val md_theme_dark_onPrimary = BrandColors.britishRacingGreen
+val md_theme_dark_onSecondary = BrandColors.britishRacingGreen3
+val md_theme_dark_primaryContainer = BrandColors.crusoe
+val md_theme_dark_background = BrandColors.darkGreen2
+val md_theme_dark_surface = BrandColors.darkGreen2
+val md_theme_dark_inverseOnSurface = BrandColors.darkGreen2
+val md_theme_dark_outline = BrandColors.delta
+val md_theme_dark_primary = BrandColors.feijoa
+val md_theme_dark_surfaceTint = BrandColors.feijoa
+val md_theme_dark_secondary = BrandColors.feijoa2
+val md_theme_dark_secondaryContainer = BrandColors.green
+val md_theme_dark_onSurfaceVariant = BrandColors.harp
+val md_theme_dark_onTertiary = BrandColors.maroon3
+val md_theme_dark_tertiaryContainer = BrandColors.maroon4
+val md_theme_dark_onError = BrandColors.maroon5
+val md_theme_dark_error = BrandColors.melon
+val md_theme_dark_onErrorContainer = BrandColors.mistyRose
+val md_theme_dark_onTertiaryContainer = BrandColors.peachPuff
+val md_theme_dark_tertiary = BrandColors.rajah
+val md_theme_dark_onPrimaryContainer = BrandColors.reef
+val md_theme_dark_onBackground = BrandColors.reef2
+val md_theme_dark_onSurface = BrandColors.reef2
+val md_theme_dark_inverseSurface = BrandColors.reef2
+val md_theme_dark_onSecondaryContainer = BrandColors.reef3
+val md_theme_dark_errorContainer = BrandColors.sangria
