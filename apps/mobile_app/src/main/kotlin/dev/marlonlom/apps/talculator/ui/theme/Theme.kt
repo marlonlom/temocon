76294eaa00366be112df8e026cf5d7a688607806
Marlon Jose Lopez Meza <malm.gm@gmail.com>
2023-04-04 11:55:54 -0500

    feat: updated sideeffect in theme definition, cause>ViewCompat.getWindowInsetsController(view) is deprecated

--- apps/mobile_app/src/main/kotlin/dev/marlonlom/apps/talculator/ui/theme/Theme.kt
+++ apps/mobile_app/src/main/kotlin/dev/marlonlom/apps/talculator/ui/theme/Theme.kt
@@ -21,7 +21,7 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 /* Light color scheme */
 private val LightColorScheme = lightColorScheme(
@@ -105,9 +105,11 @@ fun TalculatorTheme(
   }
   val view = LocalView.current
   if (!view.isInEditMode) {
+    val currentWindow = (view.context as? Activity)?.window
+      ?: throw Exception("Not in an activity - unable to get Window reference")
     SideEffect {
-      (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-      ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+      currentWindow.statusBarColor = colorScheme.primary.toArgb()
+      WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars = darkTheme
     }
   }
 
